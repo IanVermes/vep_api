c7645c61589cc189154b440b399318fb0cc0b40b
Ian Vermes <ian.vermes@gmail.com>
2020-03-21 18:48:06 +0000

    Add mypy configuration

--- vep_api/settings.py
+++ vep_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import typing as t
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = "5kb9w1y^r6&&&y3$mhht%&x^**=(kcoxx4-25*mbuq^de&l_qw"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: t.List[str] = []
 
 
 # Application definition
