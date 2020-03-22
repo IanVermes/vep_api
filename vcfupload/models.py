dc00b1a356fcbac1a1fb5d08610d8674a8cd065e
Ian Vermes <ian.vermes@gmail.com>
2020-03-22 03:54:54 +0000

    Obfuscate uploaded filenames

--- vcfupload/models.py
+++ vcfupload/models.py
@@ -1,7 +1,16 @@
+import uuid
 from django.db import models
+from pathlib import Path
 
 # Create your models here.
 
 
+def _obfuscate_uploaded_filename(_instance, filename: str) -> str:
+    ext = Path(filename).suffix
+    return str(uuid.uuid4()) + ext
+
+
 class UploadVcf(models.Model):
-    file = models.FileField(verbose_name="Uploaded Vcf")
+    file = models.FileField(
+        verbose_name="Uploaded Vcf", upload_to=_obfuscate_uploaded_filename
+    )
