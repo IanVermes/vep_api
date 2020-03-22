b71ab041dc23bd815c311bff07dd30f7c2da51db
Ian Vermes <ian.vermes@gmail.com>
2020-03-22 03:06:57 +0000

    Extend REST to use a serializer and viewset

---
+++ vcfupload_rest/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from vcfupload.models import UploadVcf
+
+
+class UploadVcfSerializer(serializers.ModelSerializer):
+    """Convert the uploaded file back and forth between Python DB model and JSON"""
+
+    class Meta:
+        model = UploadVcf
+        fields = ("pk", "file")
--- vcfupload_rest/urls.py
+++ vcfupload_rest/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from vcfupload_rest.viewsets import UploadVcfViewSet
+
 router = routers.DefaultRouter()
+router.register("vcfs", UploadVcfViewSet, "vcfs")
 
 urlpatterns = [path(r"", include(router.urls))]
