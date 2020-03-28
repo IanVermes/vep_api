a3067079445c49a1c4f823f33143f32b2171ce0b
Ian Vermes <ian.vermes@gmail.com>
2020-03-28 06:06:03 +0000

    Add minimal front end

--- webvep/main/settings.py
+++ webvep/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "webvep_frontend",
 ]
 
 MIDDLEWARE = [
---
+++ webvep/webvep_frontend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebvepFrontendConfig(AppConfig):
+    name = 'webvep_frontend'
