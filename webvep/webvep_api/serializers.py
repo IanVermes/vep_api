58ef7fb09099b5b8fc2deb632db37df206ab2a41
Ian Vermes <ian.vermes@gmail.com>
2020-03-28 20:06:38 +0000

    Add validation method for Ping serializer
    Pass unit tests

--- webvep/webvep_api/serializers.py
+++ webvep/webvep_api/serializers.py
@@ -4,5 +4,10 @@ from rest_framework import serializers
 class PingSerializer(serializers.Serializer):
     """Serializer for JSON fields"""
 
-    data = serializers.CharField()
+    data = serializers.CharField(required=True)
 
+    def validate_data(self, value):
+        """Check that the data field is specific value."""
+        if value != "ping":
+            raise serializers.ValidationError("field must be `ping`")
+        return value
