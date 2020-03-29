25682e957b3ce29f89a08c8437a5b4e65103d13a
Ian Vermes <ian.vermes@gmail.com>
2020-03-29 00:20:28 +0000

    Add unit test for VcfSerializer

--- tests/unit/webvep_api/test_serializers.py
+++ tests/unit/webvep_api/test_serializers.py
@@ -1,9 +1,10 @@
 import pytest
+from django.core.files import File
 
-from webvep_api.serializers import PingSerializer
+from webvep_api.serializers import PingSerializer, VcfSerializer
 
 
-def test_Ping_serializer_validates():
+def test_PingSerializer_validates():
     # Given
     valid_data = {"data": "ping"}
 
@@ -22,9 +23,35 @@ def test_Ping_serializer_validates():
         pytest.param({}, id="empty data", marks=pytest.mark.xfail),
     ],
 )
-def test_Ping_serializer_invalidates_bad_data(invalid_data):
+def test_PingSerializer_invalidates_bad_data(invalid_data):
     # When
     serlializer = PingSerializer(data=invalid_data)
 
     # Then
     assert serlializer.is_valid()
+
+
+def test_VcfSerializer_validates_valid_files(valid_vcf_file):
+    # Given
+    with open(valid_vcf_file, "rb") as handle:
+        django_file = File(handle, name=valid_vcf_file.name)
+    data = {"vcf_file": django_file}
+
+    # When
+    serlializer = VcfSerializer(data=data)
+
+    # Then
+    assert serlializer.is_valid()
+
+
+def test_VcfSerializer_invalidates_bad_filename(invalid_vcf_file):
+    # Given
+    with open(invalid_vcf_file, "rb") as handle:
+        django_file = File(handle, name=invalid_vcf_file.name)
+    data = {"vcf_file": django_file}
+
+    # When
+    serlializer = VcfSerializer(data=data)
+
+    # Then
+    assert not serlializer.is_valid()
