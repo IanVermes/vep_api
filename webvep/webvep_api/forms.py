5ca3bbaa763ab3d422ec54a0878ba1e3480893e6
Ian Vermes <ian.vermes@gmail.com>
2020-03-29 14:03:34 +0100

    Add VepForm dataclass to represent the variant effect output file

--- webvep/webvep_api/forms.py
+++ webvep/webvep_api/forms.py
@@ -9,3 +9,14 @@ class VcfForm:
 
     filename: str
     content: bytes
+
+
+@dataclass
+class VepForm:
+    """Representation of the VepFile i.e. variant effect output of the VEP script"""
+
+    raw_data: bytes
+    error: str
+
+    def is_valid(self):
+        return bool(self.raw_data)
